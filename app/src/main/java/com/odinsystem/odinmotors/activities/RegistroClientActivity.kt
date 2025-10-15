95462934801a2a34daaa19d317f0175a103fdf60
william quimbay <quimbayfeo@gmail.com>
2025-10-14 20:39:25 -0500

    Initial commit

---
+++ app/src/main/java/com/odinsystem/odinmotors/activities/RegistroClientActivity.kt
@@ -0,0 +1,21 @@
+package com.odinsystem.odinmotors.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.odinsystem.odinmotors.R
+
+class RegistroClientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registro_client)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/odinsystem/odinmotors/activities/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.odinsystem.odinmotors.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.odinsystem.odinmotors.MainActivity
+import com.odinsystem.odinmotors.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Evita que el usuario regrese al splash
+       }, 4000) // 2 segundos de espera
+   }
+
+}
